da1b66d6b7ce04a937423bd22bb9bc93f5b12c31
kushi19944 <57084461+kushi19944@users.noreply.github.com>
2020-07-02 11:35:41 +0900

    Update sample.ts

--- sample.ts
+++ sample.ts
@@ -1,15 +1,32 @@
 import RPA from 'ts-rpa';
 
 async function Start() {
-  await RPA.WebBrowser.get('https://www.google.com/');
-  const input = await RPA.WebBrowser.findElement('.gLFyf');
-  await RPA.WebBrowser.sendKeys(input, ['RPA']);
-  const element = await RPA.WebBrowser.findElement(
-    '.FPdoLc > center:nth-child(1) > input:nth-child(2)'
-  );
-  await RPA.WebBrowser.mouseClick(element);
+  // get() の中にURLを入れる
+  await RPA.WebBrowser.get('http://example.selenium.jp/reserveApp/');
+  // 3000で、3秒待機する
   await RPA.sleep(3000);
-  //await RPA.WebBrowser.takeScreenshot();
+
+  // "人数"の入力欄を取得する
+  const HeadCountInput = await RPA.WebBrowser.findElementById('headcount');
+  // "人数"の入力欄に文字が入っているので消す
+  await HeadCountInput.clear();
+  await RPA.sleep(1000);
+  // "人数"の入力欄に "2" を入力
+  await RPA.WebBrowser.sendKeys(HeadCountInput, ['2']);
+  await RPA.sleep(1000);
+
+  // "お名前"の入力欄を取得する
+  const NameInput = await RPA.WebBrowser.findElementById('guestname');
+  // "お名前"の入力欄に "田中" を入力
+  await RPA.WebBrowser.sendKeys(NameInput, ['田中']);
+  await RPA.sleep(1000);
+
+  // "次へ"のボタンを押す
+  const NextButton = await RPA.WebBrowser.findElementById('goto_next');
+  await RPA.WebBrowser.mouseClick(NextButton);
+
+  // ブラウザを閉じる
+  await RPA.sleep(3000);
   await RPA.WebBrowser.quit();
 }
 
