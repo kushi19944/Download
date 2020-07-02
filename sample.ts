d98ce3dff12fdc898246e66d02f6822cc983033a
kushi19944 <57084461+kushi19944@users.noreply.github.com>
2020-07-02 11:11:44 +0900

    Update sample.ts

--- sample.ts
+++ sample.ts
@@ -1,21 +1,16 @@
 import RPA from 'ts-rpa';
 
 async function Start() {
-  try {
-    await RPA.WebBrowser.get('https://www.google.com/');
-    const input = await RPA.WebBrowser.findElement('.gLFyf');
-    await RPA.WebBrowser.sendKeys(input, ['RPA']);
-    const element = await RPA.WebBrowser.findElement(
-      '.FPdoLc > center:nth-child(1) > input:nth-child(2)'
-    );
-    await RPA.WebBrowser.mouseClick(element);
-    await RPA.sleep(3000);
-    await RPA.WebBrowser.takeScreenshot();
-  } catch (error) {
-    RPA.SystemLogger.error(error);
-  } finally {
-    await RPA.WebBrowser.quit();
-  }
+  await RPA.WebBrowser.get('https://www.google.com/');
+  const input = await RPA.WebBrowser.findElement('.gLFyf');
+  await RPA.WebBrowser.sendKeys(input, ['RPA']);
+  const element = await RPA.WebBrowser.findElement(
+    '.FPdoLc > center:nth-child(1) > input:nth-child(2)'
+  );
+  await RPA.WebBrowser.mouseClick(element);
+  await RPA.sleep(3000);
+  await RPA.WebBrowser.takeScreenshot();
+  await RPA.WebBrowser.quit();
 }
 
 Start();
